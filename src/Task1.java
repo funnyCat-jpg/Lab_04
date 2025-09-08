f01e449a7bb0a71e009156cab95b46858c6c883b
616225 <dockus.616225@swl.k12.oh.us>
2025-09-08 08:56:04 -0400

    Task1 half complete.

--- src/Task1.java
+++ src/Task1.java
@@ -1,5 +1,23 @@
 public class Task1 {
     public static void main(String[] args) {
+        // Variables
+        int intOperandA = 6;
+        int intOperandB = 4;
+        int intSum = intOperandA + intOperandB;
+        int intDifference = intOperandA - intOperandB;
+        int intProduct = intOperandA * intOperandB;
+        int intQuotient = intOperandA / intOperandB;
+        int intModulo = intOperandA % intOperandB;
+
+        double doubleOperandA = 8.2 ;
+        double doubleOperandB = 4.5;
+        double doubleSum = doubleOperandA + doubleOperandB;
+        double doubleDifference = doubleOperandA - doubleOperandB;
+        double doubleProduct = doubleOperandA * doubleOperandB;
+        double doubleQuotient = doubleOperandA / doubleOperandB;
+        double doubleModulo = doubleOperandA % doubleOperandB;
+
+        System.out.println();
 
     }
 }
