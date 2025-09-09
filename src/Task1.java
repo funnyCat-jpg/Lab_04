e66750737443ff240c124808aaf33eb2ef7b84b7
616225 <dockus.616225@swl.k12.oh.us>
2025-09-09 08:55:42 -0400

    task 1, 2, 3, and 4 complete

--- src/Task1.java
+++ src/Task1.java
@@ -17,7 +17,20 @@ public class Task1 {
         double doubleQuotient = doubleOperandA / doubleOperandB;
         double doubleModulo = doubleOperandA % doubleOperandB;
 
+        // ints sums, diffrences, products, quotents, and modulo's
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+        System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+        System.out.println("The quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+        System.out.println("The modulo of " + intOperandA + " and " + intOperandB + " is " +  intModulo);
+
         System.out.println();
 
+        // doubles sums, diffrences, products, quotents, and modulo's
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+        System.out.println("The difference of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+        System.out.println("The quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+        System.out.println("The modulo of " + doubleOperandA + " and " + doubleOperandB + " is " +  doubleModulo);
     }
 }
--- src/Task2.java
+++ src/Task2.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scan =  new Scanner(System.in);
 
+        // Variables
+        double price;
+        double TAX = .05;
+        double total;
+
+        // price input and caculation of tax and total
+        System.out.println("What is the price of your purchase?");
+        price = scan.nextDouble();
+        total = price + (price * TAX);
+        System.out.println("Your total cost will be $" + total);
     }
 }
