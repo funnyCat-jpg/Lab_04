94e9cea7b16a5b00f468cfad157c0d590a3392b7
616225 <dockus.616225@swl.k12.oh.us>
2025-09-09 08:55:49 -0400

    task 1, 2, 3, 4, and 5 complete. Project done

--- src/Task4.java
+++ src/Task4.java
@@ -1,5 +1,15 @@
 public class Task4 {
     public static void main(String[] args) {
+        // Variables
+        int cardBalance = 5000;
+        double INTREST = .17;
+        double total;
 
+        // total caluclations and outputs
+        total = cardBalance + (cardBalance * INTREST);
+        System.out.println("The total balance of the credit card, after one month, will be $" + total + " including intrest");
+        System.out.println();
+        total += cardBalance + (cardBalance * INTREST);
+        System.out.println("The total balance of the credit card, after two months, will be $" + total + " including intrest");
     }
 }
--- src/Task5.java
+++ src/Task5.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Task5 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        // Variables
+        double f;
+        double c;
+
+        System.out.println("What is the temperature? (In fahrenheit)");
+        f = scan.nextDouble();
+        c = (f - 32)*5/9;
+
+        System.out.println();
+
+        System.out.println("The temperature " + f + " in fahrenheit, converted to celsius is " + c);
     }
 }
